f8871b4493f897f97bc43a8eaff409e80c450284
KoshiArchie <archie.koshti@ics-global.in>
2025-03-18 11:35:56 +0530

    commented seconds from stafftoken table and user page

--- frontend/src/components/User/UserPage.jsx
+++ frontend/src/components/User/UserPage.jsx
@@ -355,7 +355,7 @@ const UserHomePage = () => {
       year: "numeric",
       hour: "2-digit",
       minute: "2-digit",
-      second: "2-digit",
+      // second: "2-digit",
       hour12: false,
     });
   };
